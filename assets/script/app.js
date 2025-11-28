c72a589560338fb0ed6b7e1b504f9ba0223c7164
Daniel Zhang <traceless0714@gmail.com>
2025-11-28 17:52:21 -0600

    finishing javascript part

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,10 +1,98 @@
 'use strict';
-
-const settingdialog = document.querySelector('.setting-dialog');
-
 document.addEventListener('DOMContentLoaded', () => {
-  setTimeout(() => {
-    const cookiedialog = document.querySelector('.cookie-dialog');
-    cookiedialog.showModal();
-  }, 2000);
+  const settingdialog = document.querySelector('.setting-dialog');
+  const cookiedialog = document.querySelector('.cookie-dialog');
+  const acceptbtn = document.querySelector('.accept');
+  const settingbtn = document.querySelector('.setting');
+  const savebtn = document.querySelector('.savebtn');
+  const browsercheck = document.getElementById('browsercheck');
+  const oscheck = document.getElementById('oscheck');
+  const widthcheck = document.getElementById('widthcheck');
+  const heightcheck = document.getElementById('heightcheck');
+  const haveCookie = getCookies('userCookies');
+
+  if (!haveCookie) {
+    setTimeout(() => {
+      cookiedialog.showModal();
+    }, 2000);
+  }
+
+
+acceptbtn.addEventListener('click', () => {
+  saveAllCookies();
+  cookiedialog.close();
+});
+
+settingbtn.addEventListener('click', () => {
+  cookiedialog.close();
+  settingdialog.showModal();
+})
+
+savebtn.addEventListener('click', () => {
+  saveSelectedCookies();
+  settingdialog.close();
+})
+
+function saveAllCookies() {
+    setCookie('userCookies', 'fully-accepted');
+    setCookie('browser', getBrowser());
+    setCookie('os', getOS());
+    setCookie('screenWidth', screen.width);
+    setCookie('screenHeight', screen.height);
+  }
+
+function saveSelectedCookies() {
+    setCookie('userCookies', 'partially-accepted');
+    if (browsercheck.checked) {
+      setCookie('browser', getBrowser());
+    }
+    if (oscheck.checked) {
+      setCookie('os', getOS());
+    }
+    if (widthcheck.checked) {
+      setCookie('screenWidth', screen.width);
+    }
+    if (heightcheck.checked) {
+      setCookie('screenHeight', screen.height);
+    }
+
+    if (!browsercheck.checked && !oscheck.checked && !widthcheck.checked && !heightcheck.checked) {
+      setCookie('userCookies', 'rejected');
+    }
+  }
+
+
+
+function setCookie(name, value) {
+  const date = new Date();
+  date.setSeconds(date.getSeconds() + 20);
+  const expire = "expires=" + date.toUTCString();
+  document.cookie = `${encodeURIComponent(name)}=${encodeURIComponent(value)}; path=/; expires=${expire}; SameSite=Lax`;
+}
+
+function getCookies(name) {
+  const cookies = document.cookie.split(';');
+  for (let cookie of cookies) {
+    const [cookieName, cookieValue] = cookie.split('=').map(c => c.trim());
+    if(decodeURIComponent(cookieName) === name) return decodeURIComponent(cookieValue);
+  }
+  return null;
+}
+
+function getBrowser() {
+  const ua = navigator.userAgent;
+  if(ua.includes('Chrome')) return 'Chrome';
+  if(ua.includes('Firefox')) return 'Firefox';
+  if(ua.includes('Safari')) return 'Safari';
+  if(ua.includes('Edge')) return 'Edge';
+    return 'Unknown Browser';
+}
+
+  function getOS() {
+    const ua = navigator.userAgent;
+    if (ua.includes('Win')) return 'Windows';
+    if (ua.includes('Mac')) return 'MacOS';
+    if (ua.includes('Linux')) return 'Linux';
+    return 'Unknown OS';
+  }
 })
